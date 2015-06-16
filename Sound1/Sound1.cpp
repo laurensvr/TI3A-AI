b895f5a5a6786b7959739c9af2eb0f7466303230
laurensvr <laurens@vanriel.eu>
2015-06-16 13:05:04 +0200

    Update Sound1.cpp
    Added file writing
    Soundbuffer is now outside of the function, moved inside the class.

--- Sound1/Sound1.cpp
+++ Sound1/Sound1.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <iostream>
 #include <SFML/System.hpp>
+#include <fstream>
 
 using namespace std;
 
@@ -11,6 +12,9 @@ class Tone
 public:
 	double frequency;
 	std::string name;
+	std::string getName(){
+		return name;
+	}
 	void set(std::string a, double b)
 	{
 		name = a;
@@ -29,6 +33,9 @@ public:
 	Tone tone2;
 	Tone tone3;
 	Tone tone4;
+	std::string getName(){
+		return name;
+	}
 	void set(std::string a, Tone b, Tone c, Tone d)
 	{
 		name = a;
@@ -41,14 +48,17 @@ public:
 class rawGenerator
 {
 private:
-	const unsigned SAMPLES = 0.50 * 44100; //Zetten naar de kleinste kortste noot.
-	const unsigned SAMPLE_RATE = 44100;
-	const unsigned AMPLITUDE = 6000;
+	static const unsigned SAMPLES = 0.25 * 44100; //Zetten naar de kleinste kortste noot.
+	static const unsigned SAMPLE_RATE = 44100;
+	static const unsigned AMPLITUDE = 6000;
 	const double TWO_PI = 6.28318;
+	sf::Int16 waveOut[SAMPLES];
 public:
+	sf::SoundBuffer Buffer;
+	sf::Sound Sound;
 	int sw = 0;
-	sf::Int16 waveOut1[44100 / 2];
-	sf::Int16 waveOut2[44100 / 2];
+
+
 
 	void in(Tone, Tone, Tone);
 	void in(Tone, Tone);
@@ -60,32 +70,19 @@ public:
 
 void rawGenerator::playStream()
 {
-	sf::SoundBuffer Buffer;
-	sf::Sound Sound;
-
-
 	if (sw == 0){
-		if (!Buffer.loadFromSamples(waveOut1, 0.50 * 44100, 1, 44100)) {
+		if (!Buffer.loadFromSamples(waveOut, SAMPLES, 1, 44100)) {
 			std::cerr << "Loading failed!" << std::endl;
 			//return 1;
 		}
 		std::cout << "Sound 1" << std::endl;
 		sw = 0;
 	}
-	else
-	{
-		if (!Buffer.loadFromSamples(waveOut2, 0.50 * 44100, 1, 44100)) {
-			std::cerr << "Loading failed!" << std::endl;
-			//return 1;
-		}
-		std::cout << "Sound 2" << std::endl;
-		sw = 0;
-	}
+
 
 	Sound.setBuffer(Buffer);
 	//Sound.setLoop(true);
 	Sound.play();
-	sf::sleep(sf::milliseconds(500));
 
 
 }
@@ -103,7 +100,7 @@ void rawGenerator::in(Tone a, Tone b, Tone c)
 
 	for (unsigned i = 0; i < SAMPLES; i++)
 	{
-		waveOut1[i] = (sin(x * TWO_PI) + sin(y * TWO_PI) + sin(z * TWO_PI)) * AMPLITUDE;
+		waveOut[i] = (sin(x * TWO_PI) + sin(y * TWO_PI) + sin(z * TWO_PI)) * AMPLITUDE;
 		x += increment1;
 		y += increment2;
 		z += increment3;
@@ -123,7 +120,7 @@ void rawGenerator::in(Tone a, Tone b)
 
 	for (unsigned i = 0; i < SAMPLES; i++)
 	{
-		waveOut2[i] = (sin(x * TWO_PI) + sin(y * TWO_PI)) * AMPLITUDE;
+		waveOut[i] = (sin(x * TWO_PI) + sin(y * TWO_PI)) * AMPLITUDE;
 		x += increment1;
 		y += increment2;
 	}
@@ -142,7 +139,7 @@ void rawGenerator::in(Tone a)
 
 	for (unsigned i = 0; i < SAMPLES; i++)
 	{
-		waveOut2[i] = (sin(x * TWO_PI)) * AMPLITUDE;
+		waveOut[i] = (sin(x * TWO_PI)) * AMPLITUDE;
 		x += increment1;
 	}
 }
@@ -162,7 +159,7 @@ void rawGenerator::in(Chord a)
 
 	for (unsigned i = 0; i < SAMPLES; i++)
 	{
-		waveOut1[i] = (sin(x * TWO_PI) + sin(y * TWO_PI) + sin(z * TWO_PI)) * AMPLITUDE;
+		waveOut[i] = (sin(x * TWO_PI) + sin(y * TWO_PI) + sin(z * TWO_PI)) * AMPLITUDE;
 		x += increment1;
 		y += increment2;
 		z += increment3;
@@ -185,7 +182,7 @@ void rawGenerator::in(Chord a, Tone b)
 	for (unsigned i = 0; i < SAMPLES; i++)
 	{
 
-		waveOut1[i] = (sin(x * TWO_PI) + sin(y * TWO_PI) + sin(z * TWO_PI) + sin(w * TWO_PI)) * AMPLITUDE;
+		waveOut[i] = (sin(x * TWO_PI) + sin(y * TWO_PI) + sin(z * TWO_PI) + sin(w * TWO_PI)) * AMPLITUDE;
 		x += increment1;
 		y += increment2;
 		z += increment3;
@@ -199,7 +196,7 @@ int main()
 
 	Tone key[89];
 	double e = 1;
-	for (int i = 0; i<=88; i = i + 1){
+	for (int i = 0; i <= 88; i = i + 1){
 
 		double k = (e - 49) / 12;
 		double var = pow(2.0, k) * 440;
@@ -210,31 +207,31 @@ int main()
 	//TODO: complete chords
 	Chord chord[73];
 
-	chord[1].set("MC1", key[40-24], key[44-24], key[47-24]);
-	chord[2].set("MC#1 ", key[41-24], key[45-24], key[36-24]);
-	chord[3].set("MD1 ", key[42-24], key[46-24], key[49-24]);
-	chord[4].set("MD#1 ", key[31-24], key[35-24], key[37-24]);
-	chord[5].set("ME1 ", key[32-24], key[36-24], key[39-24]);
-	chord[6].set("MF1 ", key[33-24], key[37-24], key[40-24]);
-	chord[7].set("MF#1 ", key[34-24], key[38-24], key[41-24]);
-	chord[8].set("MG1 ", key[35-24], key[39-24], key[42-24]);
-	chord[9].set("MG#1 ", key[36-24], key[40-24], key[43-24]);
-	chord[10].set("MA1 ", key[37-24], key[41-24], key[44-24]);
-	chord[11].set("MA#1 ", key[38-24], key[42-24], key[45-24]);
-	chord[12].set("MB1 ", key[39-24], key[43-24], key[46-24]);
+	chord[1].set("MC1", key[40 - 24], key[44 - 24], key[47 - 24]);
+	chord[2].set("MC#1 ", key[41 - 24], key[45 - 24], key[36 - 24]);
+	chord[3].set("MD1 ", key[42 - 24], key[46 - 24], key[49 - 24]);
+	chord[4].set("MD#1 ", key[31 - 24], key[35 - 24], key[37 - 24]);
+	chord[5].set("ME1 ", key[32 - 24], key[36 - 24], key[39 - 24]);
+	chord[6].set("MF1 ", key[33 - 24], key[37 - 24], key[40 - 24]);
+	chord[7].set("MF#1 ", key[34 - 24], key[38 - 24], key[41 - 24]);
+	chord[8].set("MG1 ", key[35 - 24], key[39 - 24], key[42 - 24]);
+	chord[9].set("MG#1 ", key[36 - 24], key[40 - 24], key[43 - 24]);
+	chord[10].set("MA1 ", key[37 - 24], key[41 - 24], key[44 - 24]);
+	chord[11].set("MA#1 ", key[38 - 24], key[42 - 24], key[45 - 24]);
+	chord[12].set("MB1 ", key[39 - 24], key[43 - 24], key[46 - 24]);
 
-	chord[13].set("MC2 ", key[40-12], key[44-12], key[47-12]);
-	chord[14].set("MC#2 ", key[41-12], key[45-12], key[36-12]);
-	chord[15].set("MD2 ", key[42-12], key[46-12], key[49-12]);
-	chord[16].set("MD#2 ", key[31-12], key[35-12], key[37-12]);
-	chord[17].set("ME2 ", key[32-12], key[36-12], key[39-12]);
-	chord[18].set("MF2 ", key[33-12], key[37-12], key[40-12]);
-	chord[19].set("MF#2 ", key[34-12], key[38-12], key[41-12]);
-	chord[20].set("MG2 ", key[35-12], key[39-12], key[42-12]);
-	chord[21].set("MG#2 ", key[36-12], key[40-12], key[43-12]);
-	chord[22].set("MA2 ", key[37-12], key[41-12], key[44-12]);
-	chord[23].set("MA#2 ", key[38-12], key[42-12], key[45-12]);
-	chord[24].set("MB2 ", key[39-12], key[43-12], key[46-12]);
+	chord[13].set("MC2 ", key[40 - 12], key[44 - 12], key[47 - 12]);
+	chord[14].set("MC#2 ", key[41 - 12], key[45 - 12], key[36 - 12]);
+	chord[15].set("MD2 ", key[42 - 12], key[46 - 12], key[49 - 12]);
+	chord[16].set("MD#2 ", key[31 - 12], key[35 - 12], key[37 - 12]);
+	chord[17].set("ME2 ", key[32 - 12], key[36 - 12], key[39 - 12]);
+	chord[18].set("MF2 ", key[33 - 12], key[37 - 12], key[40 - 12]);
+	chord[19].set("MF#2 ", key[34 - 12], key[38 - 12], key[41 - 12]);
+	chord[20].set("MG2 ", key[35 - 12], key[39 - 12], key[42 - 12]);
+	chord[21].set("MG#2 ", key[36 - 12], key[40 - 12], key[43 - 12]);
+	chord[22].set("MA2 ", key[37 - 12], key[41 - 12], key[44 - 12]);
+	chord[23].set("MA#2 ", key[38 - 12], key[42 - 12], key[45 - 12]);
+	chord[24].set("MB2 ", key[39 - 12], key[43 - 12], key[46 - 12]);
 
 	chord[25].set("MC3 ", key[40], key[44], key[47]);
 	chord[26].set("MC#3 ", key[41], key[45], key[36]);
@@ -249,51 +246,100 @@ int main()
 	chord[35].set("MA#3 ", key[38], key[42], key[45]);
 	chord[36].set("MB3 ", key[39], key[43], key[46]);
 
-	chord[37].set("MC4 ", key[40+12], key[44+12], key[47+12]);
-	chord[38].set("MC#4 ", key[41+12], key[45+12], key[36+12]);
-	chord[39].set("MD4 ", key[42+12], key[46+12], key[49+12]);
-	chord[40].set("MD#4 ", key[31+12], key[35+12], key[37+12]);
-	chord[41].set("ME4 ", key[32+12], key[36+12], key[39+12]);
-	chord[42].set("MF4 ", key[33+12], key[37+12], key[40+12]);
-	chord[43].set("MF#4 ", key[34+12], key[38+12], key[41+12]);
-	chord[44].set("MG4 ", key[35+12], key[39+12], key[42+12]);
-	chord[45].set("MG#4 ", key[36+12], key[40+12], key[43+12]);
-	chord[46].set("MA4 ", key[37+12], key[41+12], key[44+12]);
-	chord[47].set("MA#4 ", key[38+12], key[42+12], key[45+12]);
-	chord[48].set("MB4 ", key[39+12], key[43+12], key[46+12]);
+	chord[37].set("MC4 ", key[40 + 12], key[44 + 12], key[47 + 12]);
+	chord[38].set("MC#4 ", key[41 + 12], key[45 + 12], key[36 + 12]);
+	chord[39].set("MD4 ", key[42 + 12], key[46 + 12], key[49 + 12]);
+	chord[40].set("MD#4 ", key[31 + 12], key[35 + 12], key[37 + 12]);
+	chord[41].set("ME4 ", key[32 + 12], key[36 + 12], key[39 + 12]);
+	chord[42].set("MF4 ", key[33 + 12], key[37 + 12], key[40 + 12]);
+	chord[43].set("MF#4 ", key[34 + 12], key[38 + 12], key[41 + 12]);
+	chord[44].set("MG4 ", key[35 + 12], key[39 + 12], key[42 + 12]);
+	chord[45].set("MG#4 ", key[36 + 12], key[40 + 12], key[43 + 12]);
+	chord[46].set("MA4 ", key[37 + 12], key[41 + 12], key[44 + 12]);
+	chord[47].set("MA#4 ", key[38 + 12], key[42 + 12], key[45 + 12]);
+	chord[48].set("MB4 ", key[39 + 12], key[43 + 12], key[46 + 12]);
 
-	chord[49].set("MC5 ", key[40+24], key[44+24], key[47+24]);
-	chord[50].set("MC#5 ", key[41+24], key[45+24], key[36+24]);
-	chord[51].set("MD5 ", key[42+24], key[46+24], key[49+24]);
-	chord[52].set("MD#5 ", key[31+24], key[35+24], key[37+24]);
-	chord[53].set("ME5 ", key[32+24], key[36+24], key[39+24]);
-	chord[54].set("MF5 ", key[33+24], key[37+24], key[40+24]);
-	chord[55].set("MF#5 ", key[34+24], key[38+24], key[41+24]);
-	chord[56].set("MG5 ", key[35+24], key[39+24], key[42+24]);
-	chord[57].set("MG#5 ", key[36+24], key[40+24], key[43+24]);
-	chord[58].set("MA5 ", key[37+24], key[41+24], key[44+24]);
-	chord[59].set("MA#5 ", key[38+24], key[42+24], key[45+24]);
-	chord[60].set("MB5 ", key[39+24], key[43+24], key[46+24]);
+	chord[49].set("MC5 ", key[40 + 24], key[44 + 24], key[47 + 24]);
+	chord[50].set("MC#5 ", key[41 + 24], key[45 + 24], key[36 + 24]);
+	chord[51].set("MD5 ", key[42 + 24], key[46 + 24], key[49 + 24]);
+	chord[52].set("MD#5 ", key[31 + 24], key[35 + 24], key[37 + 24]);
+	chord[53].set("ME5 ", key[32 + 24], key[36 + 24], key[39 + 24]);
+	chord[54].set("MF5 ", key[33 + 24], key[37 + 24], key[40 + 24]);
+	chord[55].set("MF#5 ", key[34 + 24], key[38 + 24], key[41 + 24]);
+	chord[56].set("MG5 ", key[35 + 24], key[39 + 24], key[42 + 24]);
+	chord[57].set("MG#5 ", key[36 + 24], key[40 + 24], key[43 + 24]);
+	chord[58].set("MA5 ", key[37 + 24], key[41 + 24], key[44 + 24]);
+	chord[59].set("MA#5 ", key[38 + 24], key[42 + 24], key[45 + 24]);
+	chord[60].set("MB5 ", key[39 + 24], key[43 + 24], key[46 + 24]);
 
-	chord[61].set("MC6 ", key[40+36], key[44+36], key[47+36]);
-	chord[62].set("MC#6 ", key[41+36], key[45+36], key[36+36]);
-	chord[63].set("MD6 ", key[42+36], key[46+36], key[49+36]);
-	chord[64].set("MD#6 ", key[31+36], key[35+36], key[37+36]);
-	chord[65].set("ME6 ", key[32+36], key[36+36], key[39+36]);
-	chord[66].set("MF6 ", key[33+36], key[37+36], key[40+36]);
-	chord[67].set("MF#6 ", key[34+36], key[38+36], key[41+36]);
-	chord[68].set("MG6 ", key[35+36], key[39+36], key[42+36]);
-	chord[69].set("MG#6 ", key[36+36], key[40+36], key[43+36]);
-	chord[70].set("MA6 ", key[37+36], key[41+36], key[44+36]);
-	chord[71].set("MA#6 ", key[38+36], key[42+36], key[45+36]);
-	chord[72].set("MB6 ", key[39+36], key[43+36], key[46+36]);
+	chord[61].set("MC6 ", key[40 + 36], key[44 + 36], key[47 + 36]);
+	chord[62].set("MC#6 ", key[41 + 36], key[45 + 36], key[36 + 36]);
+	chord[63].set("MD6 ", key[42 + 36], key[46 + 36], key[49 + 36]);
+	chord[64].set("MD#6 ", key[31 + 36], key[35 + 36], key[37 + 36]);
+	chord[65].set("ME6 ", key[32 + 36], key[36 + 36], key[39 + 36]);
+	chord[66].set("MF6 ", key[33 + 36], key[37 + 36], key[40 + 36]);
+	chord[67].set("MF#6 ", key[34 + 36], key[38 + 36], key[41 + 36]);
+	chord[68].set("MG6 ", key[35 + 36], key[39 + 36], key[42 + 36]);
+	chord[69].set("MG#6 ", key[36 + 36], key[40 + 36], key[43 + 36]);
+	chord[70].set("MA6 ", key[37 + 36], key[41 + 36], key[44 + 36]);
+	chord[71].set("MA#6 ", key[38 + 36], key[42 + 36], key[45 + 36]);
+	chord[72].set("MB6 ", key[39 + 36], key[43 + 36], key[46 + 36]);
+	/* initialize random seed: */
+	std::srand(time(NULL));
+
 	
 	rawGenerator r;
+	
+	ofstream fileSyntax, fileVariation1, fileVariation2;
+	fileSyntax.open("syntax.txt");
+	fileVariation1.open("variation1.txt");
+	fileVariation2.open("variation2.txt");
+	int rChordDuration = rand() % 3 + 1;
+	int rToneDuration = rand() % 3 + 1;
+	int rChord = rand() % 50 + 1;
+	int rTone = rand() % 88 + 1;
+	int rOctave = 0;
 
-	for (int i = 1; i < 18; i++){
-		r.in(chord[i], key[rand() % 88]);
-		r.playStream();
+	sf::Clock Clock;
+	sf::Time time;
+	while (1)
+	{
+		time = Clock.getElapsedTime();
+		if (time.asMicroseconds() >= 250000)
+		{
+			if (rChordDuration <= 0)
+			{
+				rChord = rand() % 72 + 1;
+				rChordDuration = rand() % 3 + 1;
+			}
+			if (rToneDuration <= 0)
+			{
+				rTone = rand() % 10 + 41;
+				rToneDuration = rand() % 3 + 1;
+			}
+
+			r.in(chord[rChord]); //Play a random Chord with a random duration
+			//r.in(chord[rChord], key[rTone]); //Play a random Chord + a random Tone both with a random duration
+			//r.in(key[(rand() % 88) + 1], key[(rand() % 88) + 1], key[(rand() % 88) + 1]); //Play three random keys changes every single beat.
+			//r.in(key[(rand() % 88) + 1], key[(rand() % 88) + 1]);
+			
+			fileSyntax << chord[rChord].getName() << " " << key[rTone].getName() << endl;
+			fileVariation1 << chord[rChord+1].getName() << " " << key[rTone+1].getName() << endl;
+			fileVariation2 << chord[rChord-1].getName() << " " << key[rTone-1].getName() << endl;
+			
+			r.playStream();
+			Clock.restart();
+
+			rChordDuration--;
+			rToneDuration--;
+		}
+
+
 	}
+	fileSyntax.close();
+	fileVariation1.close();
+	fileVariation2.close();
+
 	system("pause");
 	return 1;
 }
